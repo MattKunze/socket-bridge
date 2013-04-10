b15c23f443ba4cbb08a0e1accb2a8e88408922a2
Matt Kunze <matt.kunze@datasplice.com>
2013-04-09 18:39:22 -0600

    adding scaffold files

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('socketBridgeYoApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
